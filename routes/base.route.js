f6ee774b754e241c6ce00b4ba5e25930806cb3e7
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-07-21 14:08:01 +0700

    Add bucket object list

--- routes/base.route.js
+++ routes/base.route.js
@@ -1,21 +1,42 @@
-const {Router} = require('express')
+const { Router } = require("express");
 
-const { MinIOClient } = require('../models/MinIOClient')
+const { MinIOClient } = require("../models/MinIOClient");
 
-const router = Router()
+const router = Router();
 
-router.get('/', async (req, res) => {
-    const {accessKey, secretKey} = req.body
-    const mc = new MinIOClient(accessKey, secretKey)
+router.get("/", async (req, res) => {
+  const { accessKey, secretKey } = req.body;
+  const mc = new MinIOClient(accessKey, secretKey);
 
-    try {
-        const buckets = await mc.listBuckets()
-        res.send(buckets)
-    } catch (error) {
-        console.error(error)
-        res.status(401)
-        res.send(error)
-    }
-})
+  try {
+    const buckets = await mc.listBuckets();
+    res.send(buckets);
+  } catch (error) {
+    console.error(error);
+    res.status(401);
+    res.send(error);
+  }
+});
 
-module.exports = router
+router.get("/:bucketName", async (req, res) => {
+  const { accessKey, secretKey } = req.body;
+  const mc = new MinIOClient(accessKey, secretKey);
+
+  const { bucketName } = req.params;
+
+  let data = [];
+  const stream = mc.listObjects(bucketName, "", true);
+  stream.on("data", (obj) => {
+    data.push(obj);
+  });
+  stream.on("end", () => {
+    res.send(data);
+  });
+  stream.on("error", (err) => {
+    console.error(err);
+    res.status(500);
+    res.send(err);
+  });
+});
+
+module.exports = router;
