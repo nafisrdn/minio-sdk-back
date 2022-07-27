fd4ea48dd11e1ccf7c0fcc9c584ef093b003bb47
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-07-27 15:59:21 +0700

    Update remove unnecessary code

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const fileUpload = require("express-fileupload");
 const bucketeRoute = require("./routes/bucket.route");
 const authRoute = require("./routes/auth.route");
 
-const { MinIOClient } = require("./models/MinIOClient");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -20,57 +18,6 @@ app.use(express.json());
 app.use("/bucket", bucketeRoute);
 
 app.use("/auth", authRoute);
-
-// app.get('/', async (req, res) => {
-//     console.log('listing buckets...')
-
-//     const {accessKey, secretKey} = req.body
-
-//     const mc = new Minio.Client({
-//         endPoint: process.env.MINIO_END_POINT,
-//         port: parseInt(process.env.MINIO_PORT),
-//         useSSL: (process.env.MINIO_USE_SSL === 'true'),
-//         accessKey: accessKey,
-//         secretKey: secretKey
-//     });
-
-//     try {
-//         const buckets = await mc.listBuckets();
-
-//         res.send(buckets)
-//     } catch (error) {
-//         console.error(error)
-//         res.send(error)
-//     }
-
-// })
-
-// app.post(`/${bucketName}`, async (req, res) => {
-
-//     if (req.files === null) {
-//         res.status(400)
-//         res.send({message: 'File is empty'})
-
-//         return;
-//     }
-
-//     const file = req.files.file
-//     console.log(file)
-
-//     mc.putObject(bucketName, file.name, file.data, file.size, function(err, objInfo) {
-//         if(err) {
-//             console.log(err)
-
-//             res.status(500)
-//             res.send({message: err})
-//         }
-//      console.log("Success", objInfo)
-
-//      res.status(200)
-//      res.send({message: 'Uploaded', ...objInfo})
-//     })
-
-// })
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
